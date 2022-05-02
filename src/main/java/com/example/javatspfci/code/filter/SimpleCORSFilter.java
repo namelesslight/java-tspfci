34103fdefc781f4faf131c1c197462fefa5634ff
namelesslight <1826992499@qq.com>
2022-05-02 13:49:33 +0800

    loginChange

--- src/main/java/com/example/javatspfci/code/filter/SimpleCORSFilter.java
+++ src/main/java/com/example/javatspfci/code/filter/SimpleCORSFilter.java
@@ -2,6 +2,7 @@ package com.example.javatspfci.code.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -19,6 +20,8 @@ public class SimpleCORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String token = req.getHeader("token");
         HttpServletResponse res = (HttpServletResponse) response;
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
