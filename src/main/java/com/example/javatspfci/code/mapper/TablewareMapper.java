a4f755c61b97319671378fe9d7f7ba3b1fd51ed9
katechian <1958596678@qq.com>
2022-05-16 09:02:53 +0800

    登录

--- src/main/java/com/example/javatspfci/code/mapper/TablewareMapper.java
+++ src/main/java/com/example/javatspfci/code/mapper/TablewareMapper.java
@@ -4,6 +4,8 @@ import com.example.javatspfci.code.entity.po.Tableware;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.math.BigDecimal;
+
 /**
  * <p>
  *  Mapper 接口
@@ -14,5 +16,20 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface TablewareMapper extends BaseMapper<Tableware> {
+    /**
+     * 厂家添加餐具
+     * @param factory 对应厂家
+     * @param name 餐具名
+     * @param describe 餐具描述
+     * @param type 商品类别
+     * @param picture 图片路径
+     * @param size 餐具大小
+     * @param price 餐具价格
+     * @param stock 库存
+     * @param value 处理值
+     * @return
+     */
+    public Integer addTableware(String factory, String name, String describe, Integer type, String picture, String size,
+                                BigDecimal price, int stock, int value);
 
 }
